42c1ab674e0ea23bb25f5dbc5b55702e85c676bb
olgazaigraewa <ol.zaigraewa@yandex.ru>
2022-10-28 08:12:27 +0300

    Homework Methods

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,84 @@
+
+
 public class Main {
-    public static void main(String[] args) {
-        ;
+
+
+    public static void printIsLeapYear(int year) {
+        boolean leapYear = isLeapYear(year);
+        printIsLeapYearResult(year, leapYear);
     }
+
+    private static boolean isLeapYear(int year) {
+
+        return (year % 4 == 0 && year % 100 != 0 || year % 400 == 0);
+    }
+
+    private static void printIsLeapYearResult(int year, boolean leapYear) {
+        if (leapYear) {
+            System.out.println(year + " - високосный год.");
+        } else {
+            System.out.println(year + " - не високосный год.");
+        }
+
+    }
+
+    public static void setTheMobileApp(int clientOs, int clientDeviceYear) {
+
+        if (clientOs == 0 && clientDeviceYear == 2022) {
+            System.out.println("Установите приложение для IOS по ссылке.");
+        } else if ((clientOs == 0) && (clientDeviceYear < 2022)) {
+            System.out.println("Установите lite- версию приложения для IOS по ссылке.");
+        }
+        if ((clientOs == 1) && (clientDeviceYear == 2022)) {
+            System.out.println("Установите приложение для Android по ссылке.");
+        } else if ((clientOs == 1) && (clientDeviceYear < 2022)) {
+            System.out.println("Установите lite- версию приложения для Android  по ссылке.");
+        }
+    }
+
+
+    public static void printTheTotalDeliveryDays(int days, int deliveryDistance) {
+        if (deliveryDistance <= 20) {
+            System.out.println("Потребуется дней: " + days);
+        }
+
+        if (deliveryDistance > 20 && deliveryDistance <= 60) {
+            System.out.println("Потребуется дней: " + (days + 1));
+        }
+
+        if (deliveryDistance > 60 && deliveryDistance <= 100) {
+            System.out.println("Потребуется дней: " + (days + 2));
+        }
+
+
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Задача 1");
+        printIsLeapYear(2021);
+
+        System.out.println("Задача 2");
+        int clientDeviceYear = 2019;
+        int clientOs = 1;
+        setTheMobileApp(clientOs, clientDeviceYear);
+
+        System.out.println("Задача 3");
+        int deliveryDistance = 50;
+        int days = 1;
+        printTheTotalDeliveryDays(days, deliveryDistance);
+
+
+    }
+
 }
+
+
+
+
+
+
+
+
+
+
+
